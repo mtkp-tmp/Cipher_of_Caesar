071a8465549b6ad1f52f7aa7c2f99b997c340c35
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-02 22:11:42 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,2 +1,11 @@
-# you're code here
-print('test for trevis')
+import random
+
+n = random.randint(1, 25)
+
+
+def cipher(s, d):  # s только латиница
+    return [chr(ord(i) + d - 26) if (ord(i)+d > 122) & (ord(i)+d > 90) else chr(ord(i)+d) for i in s]
+
+
+ss1 = cipher('test for trevis', n)
+print('Сдвиг:', n, ss1)
