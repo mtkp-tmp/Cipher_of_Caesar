a120c91e1c8e392e38c1d6e81f670367dd7f4de8
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-03 00:44:25 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -7,7 +7,7 @@ n = random.randint(1, 25)
 
 
 def cipher(s, d):  # s только латиница
-    return [chr(ord(i) + d - 26) if ((ord(i) + d > 90) & (ord(i) + d < 117) &(ord(i) < 91)) ^ (ord(i) + d > 122) else chr(ord(i) + d) for i in s]
+    return [chr(ord(i) + d - 26) if ((ord(i) + d > 90) & (ord(i) + d < 117) &(ord(i) < 91)) | (ord(i) + d > 122) else chr(ord(i) + d) for i in s]
 
 
 ss1 = cipher('Hello, World!', n)
