4184c73d56292260d47585ac24a2ddae0f6631b8
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-03 23:09:27 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,14 +1,18 @@
 # you're code here
-print('test for trevis')
-
 import random
 
 n = random.randint(1, 25)
 
 
-def cipher(s, d):  # s только латиница
-    return [chr(ord(i) + d - 26) if ((ord(i) + d > 90) & (ord(i) + d < 117) &(ord(i) < 91)) | (ord(i) + d > 122) else chr(ord(i) + d) for i in s]
+def cipher_lat(s, d):  # s только латиница
+    return [chr(ord(i) + d - 26) if ((ord(i) + d > 90) & (ord(i) + d < 117) & (ord(i) < 91)) | (ord(i) + d > 122) else chr(ord(i) + d) for i in s]
 
 
-ss1 = cipher('Hello, World!', n)
+def cipher_kir(s, d):  # s только кириллица без букв ё, Ё
+    return [chr(ord(i) + d - 32) if ((ord(i) + d > 1071) & (ord(i) + d < 1104) & (ord(i) < 1072)) | (ord(i) + d > 1103) else chr(ord(i) + d) for i in s]
+
+
+ss1 = cipher_lat('Hello, World!', n)
 print('Сдвиг:', n, ss1)
+ss2 = cipher_kir('Привет, Мир!', n)
+print('Сдвиг:', n, ss2)
