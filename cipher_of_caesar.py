c627e0160cab052604fd16d865c2ea142febcbfd
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-04 01:26:53 +0300

    Update cipher_of_caesar.py
    поддержка лат+кир предложений

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,18 +1,57 @@
 # you're code here
+# шифр поддерживает гибридный режим, то есть можно вводить и лат и кир буквы в одном предложении
+# знаки пунктуации, цифры и буквы ё и Ё не изменяются
+
 import random
 
 n = random.randint(1, 25)
+rulit = 'абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+enlit = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
-def cipher_lat(s, d):  # s только латиница
-    return [chr(ord(i) + d - 26) if ((ord(i) + d > 90) & (ord(i) + d < 117) & (ord(i) < 91)) | (ord(i) + d > 122) else chr(ord(i) + d) for i in s]
+# s только латиница
+def cipher_lat(s, d):
+    return [(el, chr(ord(i) + d - 26)) if ((ord(i) + d > 90) & (ord(i) + d < 117) & (ord(i) < 91)) | (
+            ord(i) + d > 122) else (el, chr(ord(i) + d)) for el, i in s if i in enlit]
 
 
-def cipher_kir(s, d):  # s только кириллица без букв ё, Ё
-    return [chr(ord(i) + d - 32) if ((ord(i) + d > 1071) & (ord(i) + d < 1104) & (ord(i) < 1072)) | (ord(i) + d > 1103) else chr(ord(i) + d) for i in s]
+# s только кириллица без букв ё, Ё
+def cipher_kir(s, d):
+    return [(el, chr(ord(i) + d - 32)) if ((ord(i) + d > 1071) & (ord(i) + d < 1104) & (ord(i) < 1072)) | (
+            ord(i) + d > 1103) else (el, chr(ord(i) + d)) for el, i in s if i in rulit]
 
 
-ss1 = cipher_lat('Hello, World!', n)
-print('Сдвиг:', n, ss1)
-ss2 = cipher_kir('Привет, Мир!', n)
-print('Сдвиг:', n, ss2)
+# функция для сохранения небукв
+def cipher(s, d):
+    # 1. разбиваем предложение на латицицу, кириллицу и небуквы
+    notlit = list()
+    lCipher = list()  # lat
+    ruLits = list()  # kir
+    for i in range(0, len(s)):
+        if s[i] in enlit:
+            lCipher.append((i, s[i]))
+        elif s[i] in rulit:
+            ruLits.append((i, s[i]))
+        else:
+            notlit.append((i, s[i]))
+
+    # 2. шифруем латиницу и кириллицу отдельно
+    lCipher = cipher_lat(lCipher, d)
+    ruLits = cipher_kir(ruLits, d)
+
+    # 3. нормализуем для вывода
+    lCipher.extend(ruLits)
+    lCipher.extend(notlit)
+    lCipher.sort()
+    st = ''
+    for i, l in lCipher:
+        st += l
+
+    # 4. вывод
+    print('Сдвиг:', n, '\n', ''.join(s), ' => ', st)
+
+
+#####################################################
+lit = list('Hello, Мир!')  # сюда вводить нужное предложение (не поддерживается ё и Ё)
+cipher(lit, n)
+
