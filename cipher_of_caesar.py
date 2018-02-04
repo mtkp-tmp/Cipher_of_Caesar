b381aeeb49a0d3f63b43a0997f57524ee854fcbf
faraonovvit <31743662+faraonovvit@users.noreply.github.com>
2018-02-04 14:16:25 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,2 +1,23 @@
-# you're code here
-print('test for trevis')
+def CoC (text, offset):
+    s,offset="",offset%26
+    if offset==0:
+        return text
+    for c in text:
+        oc=ord(c)                       #Код символа без отступа
+        oc_of=oc+offset                 #Код символа с отступом
+        if 65<=oc<=90:
+            if oc_of>90:
+                s+=chr(65+oc_of-91)
+            else:
+                s+=chr(oc_of)
+        elif 97<=oc<=122:
+            if oc_of>122:
+                s+=chr(97+oc_of-123)
+            else:
+                s+=chr(oc_of)
+        else:
+            s+=c
+    return s
+
+print(CoC(".ABCDEFGHIJKLMNOPQRSTUVWXYZ___abcdefghijklmnopqrstuvwxyz.",-25))
+print(CoC(".ABCDEFGHIJKLMNOPQRSTUVWXYZ___abcdefghijklmnopqrstuvwxyz.",1))
