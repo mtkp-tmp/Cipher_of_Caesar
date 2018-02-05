4dc1309f311bdea9bb094a0b08c632e258b21c64
Polina Karpenko <36127142+Desmera@users.noreply.github.com>
2018-02-05 13:03:42 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,2 +1,7 @@
-# your code here
-print('test for trevis')
+n = int(input('Введите сдвиг '))
+print(*(chr(z + n) if 64 < z < 91 - n or 96 < z < 123 - n or
+                      1039 < z < 1072 - n or 1071 < z < 1104 - n else
+        chr(z - (26-n)) if 90 - n < z < 91 or 122 - n < z < 123 else
+        chr(z - (32-n)) if 1071 - n < z < 1072 or 1103 - n < z < 1104 else
+
+        chr(z) for z in map(ord, input('введите текст для шифрования '))), sep="")
