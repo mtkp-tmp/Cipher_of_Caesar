063ab996225fb7fc0d2c7633d20d5c0ac296b4f2
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-05 22:39:46 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,4 +1,3 @@
 litters = ' ,./?!#@"№$;:&%^''*()-_=+[]{}<>\|~`абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 d = int(input('Сдвиг: '))
 print('Зашифровано в', ('"' + ''.join([litters[(litters.index(lit) + d) % len(litters)] for lit in input('Шифруемый текст: ')]) + '"'))
-
