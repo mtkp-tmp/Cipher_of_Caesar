99246df17168e4b6917fe69d9e182fe91bd9e224
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-05 22:25:30 +0300

    максимально опитонил

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,13 +1,4 @@
-# шифрует любое предложение
+litters = ' ,./?!#@"№$;:&%^''*()-_=+[]{}<>\|~`абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+d = int(input('Сдвиг: '))
+print('Зашифровано в', ''.join([litters[(litters.index(lit) + d) % len(litters)] for lit in input('Шифруемый текст: ')]))
 
-litters = ' ,./?!#@"№$;:&%^*()-_=+[]{}<>\|~`абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-# функция для шифрования
-def cipher(s, d):
-    st = [litters[(litters.index(lit) + d) % len(litters)] for lit in s]
-    print('Сдвиг:', d, '\n', ''.join(s), ' => ', ''.join(st))
-    return st
-
-lit = list('Hello, W@rld! Привет,_Мир!')  # фраза
-n = 25  # сдвиг
-cipher(cipher(lit, n), -n)
