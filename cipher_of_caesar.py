f1c8db5c210d0f3da06ee0b5edf619ba79fb0e36
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-05 22:27:40 +0300

    максимум питонааааааааааа
    ух бля

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,4 +1,4 @@
 litters = ' ,./?!#@"№$;:&%^''*()-_=+[]{}<>\|~`абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 d = int(input('Сдвиг: '))
-print('Зашифровано в', ''.join([litters[(litters.index(lit) + d) % len(litters)] for lit in input('Шифруемый текст: ')]))
+print('Зашифровано в', ('"' + ''.join([litters[(litters.index(lit) + d) % len(litters)] for lit in input('Шифруемый текст: ')]) + '"'))
 
