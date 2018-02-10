4d75430d1ad58b8ea5157e485358a2b77ddb0bb3
Slager99 <32762977+Slager99@users.noreply.github.com>
2018-02-11 01:09:16 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,2 +1,6 @@
-# your code here
- print('test for trevis')
+def Shifr_Chezar(text,tab1):
+  return ''.join([chr(ord(elem)+tab1)for elem in text])
+
+text = input("Input your text: ")
+tab1 = int (input("Enter tab: "))
+print("Tab text: ",Shifr_Chezar(text,tab1))
