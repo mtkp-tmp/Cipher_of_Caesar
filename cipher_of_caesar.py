de8e2430f043d055fc7ce6a2bb1852e108a8ea89
Seizz <36148013+Seizz@users.noreply.github.com>
2018-06-11 20:45:08 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,3 +1,13 @@
-n, s, res = int(input("Введите сдвиг шифра")), input("Введите текст строчными буквами").strip(), ""
-for c in s: res += 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'[('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.index(c) + n) % len('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')]
+while True:
+    try: n, s, res = int(input("Введите сдвиг шифра")), input("Введите текст строчными буквами").strip(), ""
+    except ValueError: print("Ошибка типа данных")
+    else: break
+for c in s:
+    if c.islower():
+        if c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя': res += 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'[('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.index(c) + n) % len('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')]
+        elif c in 'abcdefghijklmnopqrstuvwxyz': res += 'abcdefghijklmnopqrstuvwxyz'[('abcdefghijklmnopqrstuvwxyz'.index(c) + n) % len('abcdefghijklmnopqrstuvwxyz')]
+    elif c.isupper():
+        if c in 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ': res += 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'[('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.index(c) + n) % len('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')]
+        elif c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ': res += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(c) + n) % len('ABCDEFGHIJKLMNOPQRSTUVWXYZ')]
+    else: res += c
 print('Защифрованный текст: "' + res + '"')
