94da1fd45be6877e790cfb6419bdca63d1cfab94
Seizz <36148013+Seizz@users.noreply.github.com>
2019-01-18 22:45:51 +0300

    Update caesar_map.py

--- caesar_map.py
+++ caesar_map.py
@@ -1,8 +1,8 @@
-print(''.join((lambda sh, text : [''.join((lambda sh, text : [''.join((lambda sh, text :
+''.join((lambda sh, text : [''.join((lambda sh, text : [''.join((lambda sh, text :
 ['abcdefghijklmnopqrstuvwxyz'[(i + sh % 26)] for i in map('abcdefghijklmnopqrstuvwxyz'.index, text)])(sh, l)) if ord(l) in range(97,123) else
-''.join((lambda sh, text : ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'[(i + sh) % 26] for i in map('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.index, text)])(sh, l)) for l in text])(sh, l)) 
+''.join((lambda sh, text : ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'[(i + sh) % 26] for i in map('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.index, text)])(sh, l)) if l.isalpha() else l for l in text])(sh, l))
 if ord(l) in range(65,123) else
 ''.join((lambda sh, text : [''.join((lambda sh, text : 
 ['абвгдеёжзийклмнопрстуфхцчшщъыьэюя'[(i + sh) % 33] for i in map('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.index, text)])(sh, l)) if ord(l) in range(1072,1106) else
-''.join((lambda sh, text : ['АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'[(i + sh) % 33] for i in map('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.index, text)])(sh, l)) for l in text])(sh,l))
-for l in text])(int(input('Введите сдвиг для шифрования шифром Цезаря : ')), input('Введите строку для шифрования шифром Цезаря : '))))
+''.join((lambda sh, text : ['АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'[(i + sh) % 33] for i in map('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.index, text)])(sh, l)) if l.isalpha() else l for l in text])(sh,l))
+for l in text])(int(input('Введите сдвиг для шифрования шифром Цезаря : ')), input('Введите строку для шифрования шифром Цезаря : ')))
